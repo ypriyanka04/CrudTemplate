7b80af71ccfbad54a7d4d994cf5fdacbcff7c906
priyanka <priyanka.decipherzone@gmail.com>
2024-11-06 01:11:45 +0530

    minor changes

--- src/main/java/com/myapp/crudtemplate/entity/Person.java
+++ src/main/java/com/myapp/crudtemplate/entity/Person.java
@@ -8,9 +8,7 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @Builder
